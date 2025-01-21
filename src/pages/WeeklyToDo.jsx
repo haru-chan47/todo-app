017153549d21467c3666d28392a7000053f7630b
haru-chan47 <insyirahyaakub@gmail.com>
2025-01-21 12:41:46 +0800

    updated-replit

--- src/pages/WeeklyToDo.jsx
+++ src/pages/WeeklyToDo.jsx
@@ -10,7 +10,7 @@ export default function WeeklyTodoList() {
     const [isEditing, setIsEditing] = useState(null);
     const [editTaskContent, setEditTaskContent] = useState("");
     const { currentUser } = useContext(AuthContext);
-    const apiBackendUrl = import.meta.env.VITE_API_BACKEND_LINK;
+    const apiBackendUrl = "https://96af620c-7813-4201-872f-5c47943a6e75-00-38ji603hc5va1.sisko.replit.dev";
     const api = `${apiBackendUrl}/todolist`
     const apiWeekly = `${apiBackendUrl}/todolist/weekly`;
     const userId = currentUser ? currentUser.uid : null;
